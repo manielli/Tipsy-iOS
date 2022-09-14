3a59a09f913b07fa39453ad68b42d10ba3fecca1
Mani Jafari <contact@manijafari.com>
2022-09-14 16:35:17 -0700

    Rename the ViewController to MainViewController to specify its purpose better and open the possibility for adding new ViewController for the result screen

--- Tipsy/Controllers/MainViewController.swift
+++ Tipsy/Controllers/MainViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  Tipsy
 //
 //  Created by Angela Yu on 09/09/2019.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
