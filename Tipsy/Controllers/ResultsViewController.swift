e6b42faf13d2b43d54e7c74bbab589565df1d989
Mani Jafari <contact@manijafari.com>
2022-09-15 11:42:40 -0700

    Add a new ViewController for results screen and link it to the already created UI screen for results

---
+++ Tipsy/Controllers/ResultsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ResultsViewController.swift
+//  Tipsy
+//
+//  Created by Mani-Jafari on 2022-09-15.
+//  Copyright © 2022 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
