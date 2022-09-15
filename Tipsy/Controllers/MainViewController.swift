11bddb30197661977b621218792bb8a54c957727
Mani Jafari <contact@manijafari.com>
2022-09-15 11:24:07 -0700

    Fix the percent buttons type to be UIButton rather than UIStackView and relink them to the UI elements and relink them to tipChanged interface builder action button

--- Tipsy/Controllers/MainViewController.swift
+++ Tipsy/Controllers/MainViewController.swift
@@ -11,13 +11,12 @@ import UIKit
 class MainViewController: UIViewController {
 
     @IBOutlet weak var billTotalTextField: UITextField!
-    @IBOutlet weak var zeroPctButton: UIStackView!
-    @IBOutlet weak var tenPctButton: UIStackView!
-    @IBOutlet weak var twentyPctButton: UIStackView!
+    @IBOutlet weak var zeroPctButton: UIButton!
+    @IBOutlet weak var tenPctButton: UIButton!
+    @IBOutlet weak var twentyPctButton: UIButton!
     @IBOutlet weak var splitNumberLabel: UILabel!
 
     @IBAction func tipChanged(_ sender: UIButton) {
-        print(sender)
     }
 
     @IBAction func stepperValueChanged(_ sender: UIStepper) {
