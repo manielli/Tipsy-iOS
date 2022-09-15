c1b60e0bd8488f0ca61ca1b4c728f98dca19ac61
Mani Jafari <contact@manijafari.com>
2022-09-15 12:04:42 -0700

    Add two interface builder outlets for ResultsViewController with links to the total per person label and the settings label based on which the total per person is calculated

--- Tipsy/Controllers/ResultsViewController.swift
+++ Tipsy/Controllers/ResultsViewController.swift
@@ -10,8 +10,13 @@ import UIKit
 
 class ResultsViewController: UIViewController {
 
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var settingsLabel: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
+    @IBAction func recalculatePressed(_ sender: UIButton) {
+    }
 }
